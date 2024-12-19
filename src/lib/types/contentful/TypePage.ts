dd9f714d3d2ab0c74a1e4845ef7d142d0693e462
aromiko <mikoaro1212@gmail.com>
2024-12-19 11:41:28 +0800

    Initial contentful types

---
+++ src/lib/types/contentful/TypePage.ts
@@ -0,0 +1,14 @@
+import type { ChainModifiers, Entry, EntryFieldTypes, EntrySkeletonType, LocaleCode } from "contentful";
+
+export interface TypePageFields {
+    name: EntryFieldTypes.Symbol;
+    pageSlug: EntryFieldTypes.Symbol;
+}
+
+export type TypePageSkeleton = EntrySkeletonType<TypePageFields, "page">;
+export type TypePage<Modifiers extends ChainModifiers, Locales extends LocaleCode = LocaleCode> = Entry<TypePageSkeleton, Modifiers, Locales>;
+export type TypePageWithoutLinkResolutionResponse = TypePage<"WITHOUT_LINK_RESOLUTION">;
+export type TypePageWithoutUnresolvableLinksResponse = TypePage<"WITHOUT_UNRESOLVABLE_LINKS">;
+export type TypePageWithAllLocalesResponse<Locales extends LocaleCode = LocaleCode> = TypePage<"WITH_ALL_LOCALES", Locales>;
+export type TypePageWithAllLocalesAndWithoutLinkResolutionResponse<Locales extends LocaleCode = LocaleCode> = TypePage<"WITHOUT_LINK_RESOLUTION" | "WITH_ALL_LOCALES", Locales>;
+export type TypePageWithAllLocalesAndWithoutUnresolvableLinksResponse<Locales extends LocaleCode = LocaleCode> = TypePage<"WITHOUT_UNRESOLVABLE_LINKS" | "WITH_ALL_LOCALES", Locales>;
---
+++ src/lib/types/contentful/index.ts
@@ -0,0 +1,1 @@
+export type { TypePage, TypePageFields, TypePageSkeleton, TypePageWithAllLocalesAndWithoutLinkResolutionResponse, TypePageWithAllLocalesAndWithoutUnresolvableLinksResponse, TypePageWithAllLocalesResponse, TypePageWithoutLinkResolutionResponse, TypePageWithoutUnresolvableLinksResponse } from "./TypePage";
