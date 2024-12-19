312cbc0fef506b4a7d0936eaa562f0c8b266a006
aromiko <mikoaro1212@gmail.com>
2024-12-19 10:14:51 +0800

    Add next config for gql files and cfassets

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,32 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(graphql|gql)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "graphql-tag/loader"
+      }
+    });
+
+    return config;
+  },
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.ctfassets.net",
+        port: ""
+      },
+      {
+        protocol: "https",
+        hostname: "videos.ctfassets.net",
+        port: ""
+      }
+    ]
+  }
 };
 
 export default nextConfig;
