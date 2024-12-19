461406619df7a89a46a54a3d7206656e7696d5dd
aromiko <mikoaro1212@gmail.com>
2024-12-19 11:53:20 +0800

    Add app defaults

---
+++ src/lib/defaults/default-app.settings.ts
@@ -0,0 +1,4 @@
+export const DefaultAppSettings = {
+  contentfulEnvironment: "master",
+  contentfulGraphqlEndpoint: "https://graphql.contentful.com/content/v1"
+};
---
+++ src/lib/defaults/default-seo-contents.ts
@@ -0,0 +1,8 @@
+export const DefaultSEOContents = {
+  seoDefaultTitle: "Default Title",
+  seoDefaultDescription: "Default Desc",
+  seoNotFoundTitle: "Not Found",
+  seoNotFoundDescription: "Page Not Found",
+  seoOgUrl: "https://www.domain.com",
+  seoOgType: "website"
+};
