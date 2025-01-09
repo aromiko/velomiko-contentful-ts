ebdb562ef8774eddf88520651145b1cf986a38dc
aromiko <mikoaro1212@gmail.com>
2025-01-09 14:50:59 +0800

    Update styles

--- src/components/component-blocks/footer/footer.tsx
+++ src/components/component-blocks/footer/footer.tsx
@@ -1,9 +1,15 @@
 import { TypeComponentFooter } from "@/lib/types/contentful";
 
+import styles from "./footer.module.css";
+
 type FooterProps = {
   data: TypeComponentFooter<"WITHOUT_UNRESOLVABLE_LINKS", string>["fields"];
 };
 
 export default function Footer({ data }: FooterProps) {
-  return <div>{data.footerName}</div>;
+  return (
+    <div className={styles.footerWrapper}>
+      <div className={styles.footerContents}>{data.footerName}</div>
+    </div>
+  );
 }
