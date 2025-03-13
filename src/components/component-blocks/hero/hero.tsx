69f164d2dfc54b1f4f3d510adb1bd25349b4d2fc
aromiko <mikoaro1212@gmail.com>
2025-03-13 12:31:36 +0800

    Initial Hero

--- src/components/component-blocks/hero/hero.tsx
+++ src/components/component-blocks/hero/hero.tsx
@@ -5,7 +5,21 @@ type HeroProps = {
 };
 
 function Hero({ data }: HeroProps) {
-  return <div>{data.heroTitle && data.heroTitle}</div>;
+  return (
+    <section
+      style={{ backgroundImage: `url(${data.heroImage.basicMediaImage.url})` }}
+      className="flex flex-col flex-1 bg-cover bg-top bg-no-repeat relative"
+    >
+      <div className="absolute h-full w-full top-0 left-0 bg-black bg-opacity-30 z-0"></div>
+      <div className="container py-20 px-4 z-10">
+        {data.heroTitle && (
+          <h1 className="font-extrabold text-4xl text-white">
+            {data.heroTitle}
+          </h1>
+        )}
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
